6bf94e3edd53fed9a600f8ec53ffaffac75111c9
Zhurakovskiy Roman <roman.zhurakovskiy@agcru.ad>
2024-12-18 09:32:21 +0300

    new project. first commit

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+
+import { initParralaxAnimation } from './modules/parallaxAnimation.js';
+import { initDistrictInteractions } from './modules/maps.js';
+
+initParralaxAnimation();
+initDistrictInteractions();
+
+
