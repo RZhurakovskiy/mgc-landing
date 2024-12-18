e9db41721c721ff37be49587d2ac703813cd81bb
Zhurakovskiy Roman <roman.zhurakovskiy@agcru.ad>
2024-12-18 10:17:35 +0300

    Добавлено плавное появления кнопки мобильного меню после прокрутки шаписки сайта. Также добавлено закрытие мобильного меню при клике на ссылки

--- javascript/modules/mobileMenu.js
+++ javascript/modules/mobileMenu.js
@@ -1,19 +1,34 @@
-export function initializeMobileMenu() {
-    console.log("ok");
-    
-    const mobileDivs = document.querySelectorAll('.mobile div');
-    const mobileNav = document.querySelector('.mobile nav');
-    const mobileNavListItems = document.querySelectorAll('.mobile ul li');
+document.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    const mobileButton = document.querySelector('.mobile div');
 
-    mobileDivs.forEach(div => {
-        div.addEventListener('click', () => {
-            mobileDivs.forEach(d => d.classList.toggle('active'));
-            mobileNav.classList.toggle('open');
-            mobileNav.querySelector('ul').classList.toggle('show');
+    if (window.scrollY > header.offsetHeight) {
+        mobileButton.style.opacity = '1';
+        mobileButton.style.transition = 'opacity 0.1s ease';
+    } else {
+        mobileButton.style.opacity = '0';
+    }
+});
+
+export function initializeMobileMenu() { 
+    const mobileDivs = document.querySelectorAll('.mobile div'); 
+    const mobileNav = document.querySelector('.mobile nav'); 
+    const mobileNavListItems = document.querySelectorAll('.mobile ul li'); 
+
+    mobileDivs.forEach(div => { 
+        div.addEventListener('click', () => { 
+            mobileDivs.forEach(d => d.classList.toggle('active')); 
+            mobileNav.classList.toggle('open'); 
+            mobileNav.querySelector('ul').classList.toggle('show'); 
+        }); 
+    }); 
+
+    mobileNavListItems.forEach((item, index) => { 
+        item.style.animationDelay = `${(index + 0.1) / 5}s`; 
+        item.addEventListener('click', () => { 
+            mobileDivs.forEach(d => d.classList.remove('active')); 
+            mobileNav.classList.remove('open'); 
+            mobileNav.querySelector('ul').classList.remove('show'); 
         });
-    });
-
-    mobileNavListItems.forEach((item, index) => {
-        item.style.animationDelay = `.${index + 2}s`;
-    });
+    }); 
 }
