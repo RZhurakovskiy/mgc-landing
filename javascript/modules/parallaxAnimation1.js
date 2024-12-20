5131f90b9df11a26ea9288cced83214736f3fdab
Zhurakovskiy Roman <roman.zhurakovskiy@agcru.ad>
2024-12-20 08:32:51 +0300

    Правки

--- javascript/modules/parallaxAnimation1.js
+++ javascript/modules/parallaxAnimation1.js
@@ -1,1 +1,16 @@
-let lastScrollY=0;let ticking=!1;export function initParralaxAnimation1(){document.addEventListener("scroll",()=>{const scrollPosition=window.scrollY;if(!ticking){window.requestAnimationFrame(()=>{const advantagesLogo=document.querySelector(".advantages-logo1");lastScrollY+=(scrollPosition-lastScrollY)*0.1;advantagesLogo.style.backgroundPositionY=`${lastScrollY * 0.1}px`;ticking=!1});ticking=!0}})}
+let lastScrollY = 0;
+let ticking = !1;
+export function initParralaxAnimation1() {
+    document.addEventListener("scroll", () => {
+        const scrollPosition = window.scrollY;
+        if (!ticking) {
+            window.requestAnimationFrame(() => {
+                const advantagesLogo = document.querySelector(".advantages-logo1");
+                lastScrollY += (scrollPosition - lastScrollY) * 0.1;
+                advantagesLogo.style.backgroundPositionY = `${lastScrollY * 0.1}px`;
+                ticking = !1;
+            });
+            ticking = !0;
+        }
+    });
+}
