5beaf3803af7148c8323c93d9a8e90cae3e229a1
unknown <rzhurakovskiy.webdev@yandex.ru>
2024-12-22 21:30:37 +0300

    Правки

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,21 @@
 import { initializePreloader } from "./modules/preloader.js";
 import { initializeMobileMenu } from "./modules/mobileMenu.js";
 import { animateHeader } from "./modules/animateHeader.js";
-import { initParralaxAnimation1 } from "./modules/parallaxAnimation1.js";
-import { initParralaxAnimation2 } from "./modules/parallaxAnimation2.js";
+import { initParralaxAnimation } from "./modules/parallaxAnimation.js";
 import { initDistrictInteractions } from "./modules/maps.js";
 
 import { animateEngineering } from "./modules/animateEngineering.js";
 import { animateInteriorExterior } from "./modules/animateInteriorExterior.js";
-import { animateInnovativeAutoglass } from "./modules/animateInnovativeAutoglass.js"; 
+import { animateInnovativeAutoglass } from "./modules/animateInnovativeAutoglass.js";
 import { animateTechnological } from "./modules/animateTechnological.js";
-import { animanteTechnologyArt } from "./modules/animanteTechnologyArt.js"; 
-import { animateMgcSystems } from "./modules/animateMgcSystems.js"; 
+import { animanteTechnologyArt } from "./modules/animanteTechnologyArt.js";
+import { animateMgcSystems } from "./modules/animateMgcSystems.js";
 
 initializePreloader();
 initializeMobileMenu();
 animateHeader();
-initParralaxAnimation1();
-initParralaxAnimation2();
+initParralaxAnimation();
+
 initDistrictInteractions();
 
 animateEngineering();
