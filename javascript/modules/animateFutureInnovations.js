be4137f684a09409af1fe1e591f97aa9e09af1e5
Zhurakovskiy Roman <roman.zhurakovskiy@agcru.ad>
2024-12-23 11:37:49 +0300

    Минифицированы файлы js

--- javascript/modules/animateFutureInnovations.js
+++ javascript/modules/animateFutureInnovations.js
@@ -1,65 +1,21 @@
-export function animateFutureInnovations() {
-  const title = document.querySelector(".future-innovations-title");
-  const animatedElements = document.querySelectorAll(
-    ".future-innovations-item ul li"
-  );
-
-  function animateTitle() {
-    const rect = title.getBoundingClientRect();
-    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-      title.classList.add("animate-title");
-
-      window.removeEventListener("scroll", animateTitle);
-    }
-  }
-
-  function animateLists() {
-    let delay = 0;
-    animatedElements.forEach((element) => {
-      const rect = element.getBoundingClientRect();
-      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-        setTimeout(() => {
-          element.classList.add("animate");
-        }, delay);
-        delay += 50;
-      }
-    });
-
-    if (
-      document.querySelectorAll(".future-innovations-item ul li:not(.animate)")
-        .length === 0
-    ) {
-      window.removeEventListener("scroll", animateLists);
-    }
-  }
-
-  function addAnimationStyles() {
-    const style = document.createElement("style");
-    style.innerHTML = `  
-          .future-innovations-title {  
-            opacity: 0;  
-            transform: translateY(-30px);  
-            transition: opacity 0.4s ease-out, transform 0.4s ease-out;  /* Уменьшена длительность анимации */
-          }  
-          .future-innovations-title.animate-title {  
-            opacity: 1;  
-            transform: translateY(0);  
-          }  
-           
-          .future-innovations-item ul li {  
-            opacity: 0;  
-            transform: translateY(20px);  
-            transition: opacity 0.4s ease-out, transform 0.4s ease-out;  /* Уменьшена длительность анимации */
-          }  
-          .future-innovations-item ul li.animate {  
-            opacity: 1;  
-            transform: translateY(0);  
-          }  
-        `;
-    document.head.appendChild(style);
-  }
-
-  addAnimationStyles();
-  window.addEventListener("scroll", animateTitle);
-  window.addEventListener("scroll", animateLists);
-}
+export function animateFutureInnovations(){let t=document.querySelector(".future-innovations-title"),e=document.querySelectorAll(".future-innovations-item ul li");function n(){let e=t.getBoundingClientRect();e.top>=0&&e.bottom<=window.innerHeight&&(t.classList.add("animate-title"),window.removeEventListener("scroll",n))}function i(){let t=0;e.forEach(e=>{let n=e.getBoundingClientRect();n.top>=0&&n.bottom<=window.innerHeight&&(setTimeout(()=>{e.classList.add("animate")},t),t+=50)}),0===document.querySelectorAll(".future-innovations-item ul li:not(.animate)").length&&window.removeEventListener("scroll",i)}!function t(){let e=document.createElement("style");e.innerHTML=`  
+    .future-innovations-title {  
+      opacity: 0;  
+      transform: translateY(-30px);  
+      transition: opacity 0.4s ease-out, transform 0.4s ease-out;  /* Уменьшена длительность анимации */
+    }  
+    .future-innovations-title.animate-title {  
+      opacity: 1;  
+      transform: translateY(0);  
+    }  
+     
+    .future-innovations-item ul li {  
+      opacity: 0;  
+      transform: translateY(20px);  
+      transition: opacity 0.4s ease-out, transform 0.4s ease-out;  /* Уменьшена длительность анимации */
+    }  
+    .future-innovations-item ul li.animate {  
+      opacity: 1;  
+      transform: translateY(0);  
+    }  
+  `,document.head.appendChild(e)}(),window.addEventListener("scroll",n),window.addEventListener("scroll",i)}
--- javascript/modules/parallaxAnimation.js
+++ javascript/modules/parallaxAnimation.js
@@ -1,16 +1,1 @@
-let lastScrollY = 0;
-let ticking = !1;
-export function initParralaxAnimation() {
-    document.addEventListener("scroll", () => {
-        const scrollPosition = window.scrollY;
-        if (!ticking) {
-            window.requestAnimationFrame(() => {
-                const advantagesLogo = document.querySelector(".advantages-logo");
-                lastScrollY += (scrollPosition - lastScrollY) * 0.1;
-                advantagesLogo.style.backgroundPositionY = `${lastScrollY * 0.1}px`;
-                ticking = !1;
-            });
-            ticking = !0;
-        }
-    });
-}
+let lastScrollY=0,ticking=!1;export function initParralaxAnimation(){document.addEventListener("scroll",()=>{let l=window.scrollY;ticking||(window.requestAnimationFrame(()=>{let t=document.querySelector(".advantages-logo");lastScrollY+=(l-lastScrollY)*.1,t.style.backgroundPositionY=`${.1*lastScrollY}px`,ticking=!1}),ticking=!0)})}
