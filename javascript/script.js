b0a780e6a4d2f3fce2d4084dba61401bed8b1b57
Zhurakovskiy Roman <roman.zhurakovskiy@agcru.ad>
2024-12-23 15:17:50 +0300

    Правки

--- javascript/script.js
+++ javascript/script.js
@@ -3,24 +3,18 @@ import { initializeMobileMenu } from "./modules/mobileMenu.js";
 import { animateHeader } from "./modules/animateHeader.js";
 import { initParralaxAnimation } from "./modules/parallaxAnimation.js";
 import { initDistrictInteractions } from "./modules/maps.js";
-
-import { animateEngineering } from "./modules/animateEngineering.js";
 import { animateInteriorExterior } from "./modules/animateInteriorExterior.js";
 import { animateInnovativeAutoglass } from "./modules/animateInnovativeAutoglass.js";
 import { animateTechnological } from "./modules/animateTechnological.js";
 import { animanteTechnologyArt } from "./modules/animanteTechnologyArt.js";
 import { animateMgcSystems } from "./modules/animateMgcSystems.js";
-
 import { animateFutureInnovations } from "./modules/animateFutureInnovations.js";
 
 initializePreloader();
 initializeMobileMenu();
 animateHeader();
 initParralaxAnimation();
-
 initDistrictInteractions();
-
-animateEngineering();
 animateInteriorExterior();
 animateInnovativeAutoglass();
 animateTechnological();
