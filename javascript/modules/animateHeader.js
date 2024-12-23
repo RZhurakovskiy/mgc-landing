c6de463ab91511ce0996be53a7761c687ccc75bf
Zhurakovskiy Roman <roman.zhurakovskiy@agcru.ad>
2024-12-23 11:17:54 +0300

    Правки

--- javascript/modules/animateHeader.js
+++ javascript/modules/animateHeader.js
@@ -1,1 +1,32 @@
-export function animateHeader(){const header=document.getElementById("main");const observer=new MutationObserver(()=>{const preloader=document.getElementById("preloader");if (preloader.classList.contains("hidden")){observer.disconnect();startHeaderAnimation(header)}});observer.observe(document.body,{subtree:true,attributes:true})}function startHeaderAnimation(header){const elementsToAnimate=header.querySelectorAll("*");elementsToAnimate.forEach((element,index)=>{setTimeout(()=>{element.style.opacity="1";element.style.transform="translateY(0)";element.style.transition="opacity 0.5s ease, transform 0.5s ease";element.style.transformOrigin="center"},index * 200)})}document.getElementById("main").querySelectorAll("*").forEach((element)=>{element.style.opacity="0";element.style.transform="translateY(20px)"});
+export function animateHeader() { 
+    const header = document.getElementById("main"); 
+    const observer = new MutationObserver(() => { 
+      const preloader = document.getElementById("preloader"); 
+      if (preloader.classList.contains("hidden")) { 
+        observer.disconnect(); 
+        startHeaderAnimation(header); 
+      } 
+    }); 
+    observer.observe(document.body, { subtree: true, attributes: true }); 
+  } 
+  
+  function startHeaderAnimation(header) { 
+    const elementsToAnimate = header.querySelectorAll("*"); 
+    elementsToAnimate.forEach((element, index) => { 
+      setTimeout(() => { 
+        element.style.opacity = "1"; 
+        element.style.transform = "translateY(0)"; 
+        element.style.transition = "opacity 0.3s ease, transform 0.3s ease";
+        element.style.transformOrigin = "center"; 
+      }, index * 100);
+    }); 
+  } 
+  
+  document 
+    .getElementById("main") 
+    .querySelectorAll("*") 
+    .forEach((element) => { 
+      element.style.opacity = "0"; 
+      element.style.transform = "translateY(20px)"; 
+    });
+  
